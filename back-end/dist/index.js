916cf32d2d6e0e596ed63c660ea18057c3ee4994
BuddhiEash <buddhieash@gmail.com>
2022-12-16 09:06:42 +1100

    Adding pgsql connection and adding dotenv variables

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Hello Typescript");
+const { Pool, Client } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
+const query = {
+    // give the query a unique name
+    name: 'fetch-apartment',
+    text: 'SELECT * FROM apartment WHERE id = $1',
+    values: ['3a73a4e5-e9e5-4628-b928-8c580778657d'],
+};
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+client.connect();
+// promise
+client
+    .query(query)
+    .then((res) => console.log(res.rows[0].name))
+    .catch((e) => console.error(e.stack));
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello Typescript")
+declare var require: any
+declare var process: any
+const { Pool, Client } = require('pg')
+const dotenv = require('dotenv')
+dotenv.config();
+ 
+const query = {
+  // give the query a unique name
+  name: 'fetch-apartment',
+  text: 'SELECT * FROM apartment WHERE id = $1',
+  values: ['3a73a4e5-e9e5-4628-b928-8c580778657d'],
+}
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+client.connect()
+
+// promise
+client
+  .query(query)
+  .then((res:any) => console.log(res.rows[0].name))
+  .catch((e:any) => console.error(e.stack))
